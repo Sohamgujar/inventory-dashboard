caa0a31f14342db9be28177e40c08d92f8d4ac57
soham <sohamsgujar03@gmail.com>
2025-12-20 14:05:46 +0530

    Switch Supabase from pooler to direct connection

--- db.py
+++ db.py
@@ -1,31 +1,3 @@
-import os
-import psycopg2
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-import psycopg2
-import os
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-conn = psycopg2.connect(
-    DATABASE_URL,
-    sslmode="require"
-)
-
-conn.autocommit = False
-cursor = conn.cursor()
-
-# 🔑 FIX: Force schema
-cursor.execute("SET search_path TO public;")
-conn.commit()
-
-def reset_transaction():
-    try:
-        conn.rollback()
-    except:
-        pass
-
 import psycopg2
 import os
 
